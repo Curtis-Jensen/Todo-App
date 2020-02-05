a442cde6ec6f5efd32c121638782e0d545f2ab2a
Ivanfeliper <47649807+Ivanfeliper@users.noreply.github.com>
2020-02-05 15:04:09 -0700

    Hello there
    Last update on the To do App , I love you all dudes hahahaha

---
+++ ToDo-App/ToDoApp/ToDoApp/Night.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace ToDoApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Night : ContentPage
+    {
+        public Night()
+        {
+            InitializeComponent();
+        }
+        private void Add_Activity(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new AddActivity());
+        }
+    }
+}
