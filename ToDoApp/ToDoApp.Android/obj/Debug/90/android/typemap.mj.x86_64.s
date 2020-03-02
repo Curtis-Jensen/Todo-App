822aafe9266ac671fa6eebf9d1a4f78e960b8479
jkotab <59858403+jkotab@users.noreply.github.com>
2020-03-02 16:26:57 -0700

    we're back

---
+++ ToDoApp/ToDoApp.Android/obj/Debug/90/android/typemap.mj.x86_64.s
@@ -0,0 +1,25 @@
+	/* Data SHA1: 8e87409446309ffe0bf03b5fa7fdd0cb05689b92 */
+	.file	"typemap.mj.inc"
+
+	/* Mapping header */
+	.section	.data.mj_typemap,"aw",@progbits
+	.type	mj_typemap_header, @object
+	.p2align	2
+	.global	mj_typemap_header
+mj_typemap_header:
+	/* version */
+	.long	1
+	/* entry-count */
+	.long	1421
+	/* entry-length */
+	.long	262
+	/* value-offset */
+	.long	145
+	.size	mj_typemap_header, 16
+
+	/* Mapping data */
+	.type	mj_typemap, @object
+	.global	mj_typemap
+mj_typemap:
+	.size	mj_typemap, 372303
+	.include	"typemap.mj.inc"
---
+++ ToDoApp/ToDoApp/AddListViewModel.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace ToDoApp
+{
+    class AddListViewModel
+    {
+        public ICommand AddEmployeeCommand => new Command(AddEmployee);
+        public ICommand DeleteEmployeeCommand => new Command(DeleteEmployee);
+
+        public ICommand UpDateCommand => new Command(UpDateEmployee);
+        public ObservableCollection<string> Employees { get; set; }
+        public string EmployeeName { get; set; }
+        public string SelectedEmployeeName { get; set; }
+        public AddListViewModel()
+        {
+            Employees = new ObservableCollection<string>();
+            Employees.Add("Study Xamarin Forms");
+            Employees.Add("Read the scriptures");
+            Employees.Add("Kiss your wife ");
+            Employees.Add("Time to workout");
+        }
+
+        public void AddEmployee()
+        {
+            Employees.Add(EmployeeName);
+        }
+        public void DeleteEmployee()
+        {
+            Employees.Remove(SelectedEmployeeName);
+        }
+        public void UpDateEmployee()
+        {
+            int newIndex = Employees.IndexOf(SelectedEmployeeName);
+            Employees.Remove(SelectedEmployeeName);
+
+            Employees.Add(EmployeeName);
+            int oldIndex = Employees.IndexOf(EmployeeName);
+
+            Employees.Move(oldIndex, newIndex);
+        }
+    }
+}
